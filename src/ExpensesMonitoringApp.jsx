77a83ee9192383e6ce6ba534d850b559990ad577
Jakub Chmielwski <kuba91821@gmail.com>
2024-09-26 11:31:39 +0200

    add application layout

---
+++ src/ExpensesMonitoringApp.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      {/* <NavBar/> */}
+      <main className="max-w-7xl mx-auto py-6">
+        <div className="px-4 py-6">
+          <div className="grid grid-cols1">
+            {/* <AddExpenseForm/> */}
+            {/* <ExpenseSummary/> */}
+          </div>
+          {/* <ExpenseList/> */}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
